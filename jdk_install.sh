9ebddbb4793e1eddcb882b3e749d64c79200767a
Juan Bindez <juanbindez780@gmail.com>
2024-11-21 23:07:10 -0300

    Update jdk_install.sh

--- jdk_install.sh
+++ jdk_install.sh
@@ -1,6 +1,14 @@
-
+#!/bin/bash
 
 sudo apt install openjdk-17-jdk -y
 ls /usr/lib/jvm
+
+sleep 5
+
 export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
 export PATH=$JAVA_HOME/bin:$PATH
+
+sleep 3
+
+echo $JAVA_HOME
+java -version
